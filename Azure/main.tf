a593dad706cf23ac36e69eb223e8d6788d529c5a
Elijah Beringuel <beringuel.elijah@gmail.com>
2024-12-02 08:05:49 -0500

    Azure cloud initial configuration

---
+++ Azure/main.tf
@@ -0,0 +1,139 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.12.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "f37c21fe-b3de-4af5-95a0-d88901bc6308"
+}
+
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "canadaeast"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_subnet" "mtc-subnet-01" {
+  name                 = "mtc-subnet-01"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-security-group"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "mtc-sr" {
+  name                        = "mtc-security-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "99.236.207.5/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
+  subnet_id                 = azurerm_subnet.mtc-subnet-01.id
+  network_security_group_id = azurerm_network_security_group.mtc-sg.id
+}
+
+resource "azurerm_public_ip" "mtc-ip" {
+  name                = "mtc-ip"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  allocation_method   = "Dynamic"
+  sku = "Basic"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_interface" "mtc-nic" {
+  name                = "mtc-nic"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mtc-subnet-01.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.mtc-ip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_ssh_public_key" "mtc-ssh-key" {
+  name                = "mtc-ssh"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location  
+  public_key          = file("~/.ssh/pingu.pub")
+}
+
+resource "azurerm_virtual_machine" "dev-01" {
+  name                  = "Dev-01"
+  location              = azurerm_resource_group.mtc-rg.location
+  resource_group_name   = azurerm_resource_group.mtc-rg.name
+  network_interface_ids = [azurerm_network_interface.mtc-nic.id]
+  vm_size               = "Standard_DS2_v2"
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "OS-disk-01"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "dev-01"
+    admin_username = "pingu"
+    admin_password = "Aetherpass01!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "dev"
+  }
+}
